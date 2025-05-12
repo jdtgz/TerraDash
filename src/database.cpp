f0680512e6e271d46d64eb764b7bdc6b93bb5564
Riley Carter <rileycarter79@gmail.com>
2025-05-12 15:11:00 -0500

    Database Changes

--- src/database.cpp
+++ src/database.cpp
@@ -21,13 +21,13 @@ Database::~Database() {
 }
 
 void Database::logIn(const std::string& username, const std::string& email) {
-    auto res = client.Post("/login_game", 
+    auto res = client.Post("/bridge/login_game", 
                             "{ \"username\": \"" + username + "\", \"email\": \"" + email + "\" }", 
                             "application/json");
 }
 
 void Database::uploadRun(int userId, int levelId, int score, int runTime) {
-    auto res = client.Post("/upload_run", 
+    auto res = client.Post("/bridge/upload_run",
                             "{ \"user_id\": " + std::to_string(userId) + 
                             ", \"level_id\": " + std::to_string(levelId) + 
                             ", \"score\": " + std::to_string(score) + 
